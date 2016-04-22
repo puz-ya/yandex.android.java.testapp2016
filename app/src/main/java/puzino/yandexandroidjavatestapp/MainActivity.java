d1f56f3a3623b488cc0f27907cc4bc2c68aa023a
Puzino Yury <puzino.y.a@gmail.com>
2016-04-22 14:16:51 +0300

    Добавил разрешения на выход в интернет. Первая итерация разделки json файла - получил список имён в лог.

--- app/src/main/java/puzino/yandexandroidjavatestapp/MainActivity.java
+++ app/src/main/java/puzino/yandexandroidjavatestapp/MainActivity.java
@@ -1,13 +1,95 @@
 package puzino.yandexandroidjavatestapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //устанавливаем начальный вид
+        new TestParse().execute();              //получаем данные из yandex json
     }
+
+    public static String LOG_TAG = "test_artist_log";
+
+    private class TestParse extends AsyncTask<Void, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                URL url = new URL("http://download.cdn.yandex.net/mobilization-2016/artists.json");
+
+                //соединяемся
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                //переводим в строку
+                resultJson = buffer.toString();
+
+                //закрываем соединение
+                urlConnection.disconnect();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+            //Log.d(LOG_TAG, strJson);
+
+            JSONArray dataJsonObj = null;
+
+            try {
+                dataJsonObj = new JSONArray(strJson);
+
+                // идём по всем исполнителям
+                for (int i = 0; i < dataJsonObj.length(); i++) {
+                    JSONObject artist = dataJsonObj.getJSONObject(i);
+
+                    //получаем строку с именем
+                    String name = artist.getString("name");
+
+                    Log.d(LOG_TAG, "Name: " + name);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
