bb29a616b71008dfaa8fe1cc5411c4d4bccb2ba5
Puzino Yury <puzino.y.a@gmail.com>
2016-04-23 04:58:55 +0300

    Добавлены заготовки для иконок и изображений. Добавлен файл xml для русского языка. Получаем строки-ссылки на изображения из JSON. Добавлен слой для краткого описания исполнителя.

--- app/src/main/java/puzino/yandexandroidjavatestapp/MainActivity.java
+++ app/src/main/java/puzino/yandexandroidjavatestapp/MainActivity.java
@@ -3,7 +3,11 @@ package puzino.yandexandroidjavatestapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TableLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,27 +18,36 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //устанавливаем начальный вид
         new TestParse().execute();              //получаем данные из yandex json
+
+        //
     }
 
     public static String LOG_TAG = "test_artist_log";
 
+    //ассинхронный класс для загрузки файла
+    //входные данные - нет, промежуточные - нет, возвращаемые - строка
     private class TestParse extends AsyncTask<Void, Void, String> {
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String resultJson = "";
 
-        @Override
+        //перед стартом "закачки" надо показать начальное состояние (Загрузка...)
+        //TODO: написать protected void onPostExecute()
+
+        @Override   // получаем данные с внешнего ресурса в фоне
         protected String doInBackground(Void... params) {
-            // получаем данные с внешнего ресурса
             try {
                 URL url = new URL("http://download.cdn.yandex.net/mobilization-2016/artists.json");
 
@@ -65,13 +78,15 @@ public class MainActivity extends AppCompatActivity {
             return resultJson;
         }
 
+        //после doInBackground запускаем распарсивание и заполнение UI
         @Override
         protected void onPostExecute(String strJson) {
             super.onPostExecute(strJson);
-            // выводим целиком полученную json-строку
-            //Log.d(LOG_TAG, strJson);
 
+            // получаем json-массив
             JSONArray dataJsonObj = null;
+            // список всех слоёв (карточек\блоков) с артистами
+            ListView listOfViews = (ListView) findViewById(R.id.ListViewArtistSmall);
 
             try {
                 dataJsonObj = new JSONArray(strJson);
@@ -82,8 +97,18 @@ public class MainActivity extends AppCompatActivity {
 
                     //получаем строку с именем
                     String name = artist.getString("name");
+                    //получаем маленькое и большое изображение, это JSONObject
+                    JSONObject cover = artist.getJSONObject("cover");
 
-                    Log.d(LOG_TAG, "Name: " + name);
+                    String cover_small = cover.getString("small");
+                    String cover_big = cover.getString("big");
+
+                    //TODO: идём в цикле по слою и вставляем layouts_artist_short по числу исполнителей
+                    //TODO: Заготовка для скачивания изображений исполнителей и их добавление в layout
+                    //Drawable downloadedImage = ImageGet(this, url);
+                    //image.setImageDrawable(downloadedImage);
+
+                    Log.d(LOG_TAG, "Name: " + name + cover_big);
                 }
 
             } catch (JSONException e) {
